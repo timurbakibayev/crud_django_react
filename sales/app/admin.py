f911a920ca661f629a8c06c203c29a04a7c836f4
Timur Bakibayev <timurbakibayev@gmail.com>
2021-03-10 16:26:27 +0100

    models

--- sales/app/admin.py
+++ sales/app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from app.models import Order
 
-# Register your models here.
+admin.site.register(Order)
--- sales/app/models.py
+++ sales/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    date = models.DateField(blank=False)
+    item = models.CharField(max_length=100, blank=False)
+    price = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    quantity = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    amount = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+
+    def __str__(self):
+        return f"{self.date}: {self.item}"

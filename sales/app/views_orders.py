3b7cb48ffc231ad8d09050b9bced2e7f338428ca
Timur Bakibayev <timurbakibayev@gmail.com>
2021-03-15 17:21:54 +0100

    PUT and DELETE

--- sales/app/views_orders.py
+++ sales/app/views_orders.py
@@ -17,6 +17,60 @@ def serialize_order(order):
     return serialized
 
 
+def save_order(request, order, success_status):
+    errors = []
+    item = request.data.get("item", "")
+    if item == "":
+        errors.append({"item": "This field is required"})
+
+    try:
+        price = request.data.get("price", "")
+        if price == "":
+            errors.append({"price": "This field is required"})
+        else:
+            price = int(price)
+            if price < 0:
+                errors.append({"price": "Price cannot be negative"})
+    except ValueError:
+        errors.append({"price": "Could not parse field"})
+
+    try:
+        quantity = request.data.get("quantity", "")
+        if quantity == "":
+            errors.append({"quantity": "This field is required"})
+        else:
+            quantity = int(quantity)
+            if quantity < 0:
+                errors.append({"quantity": "Quantity cannot be negative"})
+    except ValueError:
+        errors.append({"quantity": "Could not parse field"})
+
+    date = request.data.get("date", "")
+    if date == "":
+        date = datetime.datetime.now()
+
+    if len(errors) > 0:
+        return HttpResponse(json.dumps(
+            {
+                "errors": errors
+            }), status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        order.date = date
+        order.item = item
+        order.price = price
+        order.quantity = quantity
+        order.amount = price * quantity
+        order.save()
+    except Exception as e:
+        return HttpResponse(json.dumps(
+            {
+                "errors": {"Order": str(e)}
+            }), status=status.HTTP_400_BAD_REQUEST)
+
+    return HttpResponse(json.dumps({"data": serialize_order(order)}), status=success_status)
+
+
 @api_view(['GET', 'POST'])
 def orders(request):
     if request.user.is_anonymous:
@@ -28,59 +82,8 @@ def orders(request):
         return HttpResponse(json.dumps({"data": orders}), status=status.HTTP_200_OK)
 
     if request.method == "POST":
-        errors = []
-        item = request.data.get("item","")
-        if item == "":
-            errors.append({"item": "This field is required"})
-
-        try:
-            price = request.data.get("price","")
-            if price == "":
-                errors.append({"price": "This field is required"})
-            else:
-                price = int(price)
-                if price < 0:
-                    errors.append({"price": "Price cannot be negative"})
-        except ValueError:
-            errors.append({"price": "Could not parse field"})
-
-        try:
-            quantity = request.data.get("quantity","")
-            if quantity == "":
-                errors.append({"quantity": "This field is required"})
-            else:
-                quantity = int(quantity)
-                if quantity < 0:
-                    errors.append({"quantity": "Quantity cannot be negative"})
-        except ValueError:
-            errors.append({"quantity": "Could not parse field"})
-
-        date = request.data.get("date","")
-        if date == "":
-            date = datetime.datetime.now()
-
-        if len(errors) > 0:
-            return HttpResponse(json.dumps(
-                {
-                    "errors": errors
-                }), status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            order = Order()
-            order.date = date
-            order.item = item
-            order.price = price
-            order.quantity = quantity
-            order.amount = price * quantity
-            order.save()
-        except Exception as e:
-            return HttpResponse(json.dumps(
-                {
-                    "errors": {"Order": str(e)}
-                }), status=status.HTTP_400_BAD_REQUEST)
-
-        return HttpResponse(json.dumps({"data": serialize_order(order)}), status=status.HTTP_201_CREATED)
-
+        order = Order()
+        return save_order(request, order, status.HTTP_201_CREATED)
 
     return HttpResponse(json.dumps({"detail": "Wrong method"}), status=status.HTTP_501_NOT_IMPLEMENTED)
 
@@ -97,5 +100,12 @@ def order(request, order_id):
 
     if request.method == "GET":
         return HttpResponse(json.dumps({"data": serialize_order(order)}), status=status.HTTP_200_OK)
+
+    if request.method == "PUT":
+        return save_order(request, order, status.HTTP_200_OK)
+
+    if request.method == "DELETE":
+        order.delete()
+        return HttpResponse(json.dumps({"detail": "deleted"}), status=status.HTTP_410_GONE)
 
     return HttpResponse(json.dumps({"detail": "Wrong method"}), status=status.HTTP_501_NOT_IMPLEMENTED)

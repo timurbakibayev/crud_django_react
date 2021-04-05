4f894aa218f0f0f9978b8172bbd35354c79db0e2
Timur Bakibayev <timurbakibayev@gmail.com>
2021-04-05 16:36:14 +0200

    API connected

--- sales/app/static/js/api.js
+++ sales/app/static/js/api.js
@@ -25,7 +25,7 @@ const login_api = async (username, password, success, fail) => {
   }
 };
 
-const get_data_api = async (pageNo="", success, fail) => {
+const get_sales_api = async (pageNo="", success, fail) => {
   const token = await localStorage.getItem("salesToken");
   if (token === null) {
     console.log("No credentials found, redirecting...");
@@ -49,6 +49,111 @@ const get_data_api = async (pageNo="", success, fail) => {
     return [];
   }
   if (response.status === 200) {
+    console.log("success", JSON.parse(text));
+    success(JSON.parse(text));
+  } else {
+    console.log("failed", text);
+    Object.entries(JSON.parse(text)).forEach(([key, value])=>{
+      fail(`${key}: ${value}`);
+    });
+  }
+};
+
+const post_sale_api = async (data, success) => {
+  const token = await localStorage.getItem("salesToken");
+  if (token === null) {
+    console.log("No credentials found, redirecting...");
+    window.location = "/login";
+    return [];
+  }
+  const response = await fetch(
+        `/api/orders/`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'Application/JSON',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify(data)
+        }
+    );
+  const text = await response.text();
+  if (response.status === 401) {
+    console.log("Token not valid");
+    window.location = "/login";
+    return [];
+  }
+  if (response.status === 201) {
+    console.log("success", JSON.parse(text));
+    success(JSON.parse(text));
+  } else {
+    console.log("failed", text);
+    Object.entries(JSON.parse(text)).forEach(([key, value])=>{
+      fail(`${key}: ${value}`);
+    });
+  }
+};
+
+const put_sale_api = async (saleId, data, success) => {
+  const token = await localStorage.getItem("salesToken");
+  if (token === null) {
+    console.log("No credentials found, redirecting...");
+    window.location = "/login";
+    return [];
+  }
+  const response = await fetch(
+        `/api/orders/${saleId}/`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'Application/JSON',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify(data)
+        }
+    );
+  const text = await response.text();
+  if (response.status === 401) {
+    console.log("Token not valid");
+    window.location = "/login";
+    return [];
+  }
+  if (response.status === 200) {
+    console.log("success", JSON.parse(text));
+    success(JSON.parse(text));
+  } else {
+    console.log("failed", text);
+    Object.entries(JSON.parse(text)).forEach(([key, value])=>{
+      fail(`${key}: ${value}`);
+    });
+  }
+};
+
+const delete_sale_api = async (saleId, success) => {
+  const token = await localStorage.getItem("salesToken");
+  if (token === null) {
+    console.log("No credentials found, redirecting...");
+    window.location = "/login";
+    return [];
+  }
+  const response = await fetch(
+        `/api/orders/${saleId}/`,
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'Application/JSON',
+                'Authorization': `Bearer ${token}`,
+            }
+        }
+    );
+  const text = await response.text();
+  if (response.status === 401) {
+    console.log("Token not valid");
+    window.location = "/login";
+    return [];
+  }
+  console.log(response.status);
+  if (response.status === 410) {
     console.log("success", JSON.parse(text));
     success(JSON.parse(text));
   } else {
--- sales/app/static/js/sales_page.js
+++ sales/app/static/js/sales_page.js
@@ -3,6 +3,13 @@ const e = React.createElement;
 
 function App() {
   const [list, setList] = React.useState([]);
+  const [showModal, setShowModal] = React.useState(false);
+  const [modalDescription, setModalDescription] = React.useState("");
+  const [itemId, setItemId] = React.useState(null);
+  const [error, setError] = React.useState("");
+  const [item, setItem] = React.useState("");
+  const [price, setPrice] = React.useState(0);
+  const [quantity, setQuantity] = React.useState(0);
 
   const success = (data) => {
     setList(data.data);
@@ -14,13 +21,119 @@ function App() {
   };
 
   const getData = async ()=>{
-    await get_data_api(0, success, (text)=>{console.log("Error: ", text)})
+    await get_sales_api(0, success, (text)=>{console.log("Error: ", text)})
+  };
+
+  const newSale = ()=>{
+    setModalDescription("New sale");
+    setItemId(null);
+    setItem("");
+    setPrice(0);
+    setQuantity(0);
+    setError("");
+    setShowModal(true);
+    const itemInput = document.getElementById("itemInput");
+    setTimeout(()=>{itemInput && itemInput.focus()}, 1);
+  };
+
+  const editSale = (data)=>{
+    setModalDescription("New sale");
+    setItemId(data.id);
+    setItem(data.item);
+    setPrice(data.price);
+    setQuantity(data.quantity);
+    setError("");
+    setShowModal(true);
+    const itemInput = document.getElementById("itemInput");
+    setTimeout(()=>{itemInput && itemInput.focus()}, 1);
+  };
+
+  const saveSale = (e)=>{
+    e.preventDefault();
+    setError("");
+    console.log("saving new", item, price, quantity);
+    if (item.length * price * quantity === 0)
+      setError("Please enter item name, price and quantity");
+    else {
+      if (itemId === null)
+        post_sale_api({item, price, quantity}, ()=>{getData();});
+      else
+        put_sale_api(itemId, {item, price, quantity}, ()=>{getData();});
+      setShowModal(false);
+    }
+  };
+
+  const deleteSale = (saleId)=>{
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        delete_sale_api(saleId, ()=>{
+          Swal.fire({
+              title: 'Deleted!',
+              text: "Your sale has been deleted!",
+              icon: 'success',
+              timer: 1000,
+          });
+          getData();
+        });
+      }
+    });
   };
 
   React.useEffect(getData,[]);
 
   return (
     <div>
+      <div className={"modal " + (showModal?" show d-block":" d-none")} tabIndex="-1" role="dialog">
+        <div className="modal-dialog" style={{boxShadow: "8px 9px 7px 2px gray"}}>
+          <form method="post">
+          <div className="modal-content" onKeyDown={
+            (e)=>{
+            if (e.which === 27)
+              setShowModal(false);
+            }
+          }>
+            <div className="modal-header">
+              <h5 className="modal-title">{modalDescription}</h5>
+              <button type="button" className="btn-close" onClick={()=>{setShowModal(false)}} aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <label>Item name</label>
+                <div className="form-group">
+                  <input type="text" className="form-control" name="item" id="itemInput"
+                         value={item} onChange={(e)=>{setItem(e.target.value)}}
+                         placeholder="Item name"/>
+                </div>
+              <label style={{marginTop: "1em"}}>Price</label>
+                <div className="form-group" >
+                  <input type="number" className="form-control" placeholder="Price"
+                         value={price} onChange={(e)=>{setPrice(e.target.value)}}
+                         name="price" />
+                </div>
+              <label style={{marginTop: "1em"}}>Quantity</label>
+                <div className="form-group">
+                  <input type="number" className="form-control"
+                         value={quantity} onChange={(e)=>{setQuantity(e.target.value)}}
+                         placeholder="Quantity" name="quantity" />
+                </div>
+              <small className="form-text text-muted">{error}</small>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" onClick={()=>{setShowModal(false)}} data-bs-dismiss="modal">Close</button>
+              <button type="submit" className="btn btn-primary" onClick={saveSale}>Save changes</button>
+            </div>
+          </div>
+          </form>
+        </div>
+      </div>
+
       <div style={{maxWidth: "800px", margin: "auto", marginTop: "1em", marginBottom: "1em",
         boxShadow: "5px 5px 20px #cccccccc",
         padding: "1em"
@@ -35,7 +148,9 @@ function App() {
         padding: "1em"
                 }}>
         <div style={{display: "flex", flexDirection: "row", marginBottom: "5px"}}>
-          <a className="btn btn-light" style={{marginLeft: "auto"}}>New Sale</a>
+          <a className="btn btn-light" style={{marginLeft: "auto"}}
+             onClick={newSale}
+          >New Sale</a>
         </div>
         <table className="table table-hover caption-top">
           <thead className="table-light">
@@ -58,7 +173,12 @@ function App() {
               <td>{row.price}</td>
               <td>{row.quantity}</td>
               <td>{row.amount}</td>
-              <td>Edit</td>
+              <td>
+                <a className="btn btn-light" style={{marginLeft: "auto"}}
+                  onClick={(e)=>{editSale(row)}}>Edit</a>{" "}
+                <a className="btn btn-light" style={{marginLeft: "auto"}}
+                  onClick={(e)=>{deleteSale(row.id)}}>Delete</a>
+              </td>
             </tr>
           )}
           </tbody>
